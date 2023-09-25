01494c9455bd0c9f87efb2d1984f667a8e1b8980
AiDoo-S <99187300+AiDoo-S@users.noreply.github.com>
2023-09-25 19:09:38 +0200

    day 8 part 1 finished

--- 2022/day08/part1.py
+++ 2022/day08/part1.py
@@ -1,49 +1,31 @@
-from copy import deepcopy
-
 with open("input.txt", "r") as file:
-    txt = file.read().strip().split("\n")
+    grid = [list(map(int, line.strip())) for line in file.readlines()]
 
-txt = """30373
-25512
-65332
-33549
-35390""".split("\n")
-
-
-def check_visibility(value, i, j):
-    # up
-    if all(x >= value for x in txt[0:i][j]):
-        print("bad")
-    # right
-    # down
-    # left
-
-    return True
+def check_visibility(grid, r, c):
+    if c == 0 or c == C-1 or r == 0 or r == R -1:
+        return 1
+    # look left
+    if all(grid[r][cc] < grid[r][c] for cc in range(c)):
+        return 1
+    # look right
+    if all(grid[r][cc] < grid[r][c] for cc in range(c + 1, C)):
+        return 1
+    # look up
+    if all(grid[rr][c] < grid[r][c] for rr in range(r)):
+        return 1
+    # look down
+    if all(grid[rr][c] < grid[r][c] for rr in range(r + 1, R)):
+        return 1
+    
+    return 0
 
 count = 0
 
-# Count edges
-count += len(txt) * 2 + (len(txt[0]) - 2) * 2
+R = len(grid)
+C = len(grid[0])
 
-# Count visible
-
-i = 1
-j = 1
-print(txt)
-while i < len(txt[0]) - 1:
-    top_visible = False
-    right_visible = False
-    bottom_visible = False
-    left_visible = False
-    j = 1
-    while j < len(txt) - 1:
-        # print(f"i: {i}")
-        # print(f"j: {j}")
-        # print(txt[i][j])
-        if check_visibility(txt[i][j], i, j):
-            count += 1
-        j += 1
-
-    i += 1
+for r in range(R):
+    for c in range(C):
+        count += check_visibility(grid, r, c)
 
 print(count)
--- 2022/day08/part2.py
+++ 2022/day08/part2.py
@@ -1,8 +1,31 @@
 with open("input.txt", "r") as file:
-    txt = file.read().strip().split("\n")
+    grid = [list(map(int, line.strip())) for line in file.readlines()]
 
-txt = """30373
-25512
-65332
-33549
-35390""".split("\n")
+def check_visibility(grid, r, c):
+    if c == 0 or c == C-1 or r == 0 or r == R -1:
+        return 1
+    # look left
+    if all(grid[r][cc] < grid[r][c] for cc in range(c)):
+        return 1
+    # look right
+    if all(grid[r][cc] < grid[r][c] for cc in range(c + 1, C)):
+        return 1
+    # look up
+    if all(grid[rr][c] < grid[r][c] for rr in range(r)):
+        return 1
+    # look down
+    if all(grid[rr][c] < grid[r][c] for rr in range(r + 1, R)):
+        return 1
+    
+    return 0
+
+count = 0
+
+R = len(grid)
+C = len(grid[0])
+
+for r in range(R):
+    for c in range(C):
+        count += check_visibility(grid, r, c)
+
+print(count)
