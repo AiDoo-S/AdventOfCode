c35452fb0c8a45244dc4a44298e8e456c70e4627
AiDoo-S <99187300+AiDoo-S@users.noreply.github.com>
2023-09-26 19:14:24 +0200

    part 2 day 8 finished

--- 2022/day08/part2.py
+++ 2022/day08/part2.py
@@ -1,31 +1,38 @@
 with open("input.txt", "r") as file:
     grid = [list(map(int, line.strip())) for line in file.readlines()]
 
-def check_visibility(grid, r, c):
+def check_scenic_score(grid, r, c):
     if c == 0 or c == C-1 or r == 0 or r == R -1:
-        return 1
-    # look left
-    if all(grid[r][cc] < grid[r][c] for cc in range(c)):
-        return 1
-    # look right
-    if all(grid[r][cc] < grid[r][c] for cc in range(c + 1, C)):
-        return 1
-    # look up
-    if all(grid[rr][c] < grid[r][c] for rr in range(r)):
-        return 1
-    # look down
-    if all(grid[rr][c] < grid[r][c] for rr in range(r + 1, R)):
-        return 1
+        return 0
     
-    return 0
+    for dc in range(1, c + 1):
+        if grid[r][c - dc] >= grid[r][c]:
+            break
+    left = dc
 
-count = 0
+    for dc in range(1, C - c):
+        if grid[r][c + dc] >= grid[r][c]:
+            break
+    right = dc
+
+    for dr in range(1, r + 1):
+        if grid[r - dr][c] >= grid[r][c]:
+            break
+    top = dr
+
+    for dr in range(1, R - r):
+        if grid[r + dr][c] >= grid[r][c]:
+            break
+    bottom = dr
+    return left * right * top * bottom
+
+score = 0
 
 R = len(grid)
 C = len(grid[0])
 
 for r in range(R):
     for c in range(C):
-        count += check_visibility(grid, r, c)
+        score = max(score, check_scenic_score(grid, r, c))
 
-print(count)
+print(score)
