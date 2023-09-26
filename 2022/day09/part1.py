2816bfeb2c6b2d6922f394d00372070e668c8517
AiDoo-S <99187300+AiDoo-S@users.noreply.github.com>
2023-09-26 20:24:58 +0200

    day 9 part 1 finished

--- 2022/day09/part1.py
+++ 2022/day09/part1.py
@@ -1,2 +1,29 @@
+import math
+
 with open("input.txt", "r") as file:
-    txt = file.read().strip().split("\n")
+    lines = file.readlines()
+
+
+visited = {(0, 0)}
+# 0, 0 origin is top left
+move = {'L': (-1, 0), 'R': (1, 0), 'U': (0, -1), 'D': (0, 1)}
+
+hx, hy, tx, ty = 0, 0, 0, 0
+
+for instruction in lines:
+    direction, distance = instruction.split(" ")
+    m = move[direction]
+    for _ in range(int(distance)):
+        # move head
+        hx, hy = hx + m[0], hy + m[1]
+
+        # update tail
+        dx, dy = hx - tx, hy - ty
+        if abs(dx) > 1 or abs(dy) > 1:
+            if dx:
+                tx += math.copysign(1, dx)
+            if dy:
+                ty += math.copysign(1, dy)
+            visited.add((tx, ty))
+
+print(len(visited))
